d7b94eb0eb1202843a3e2d934113b4a1cf695730
AGPWR <268508@student.pwr.edu.pl>
2022-04-07 04:52:46 +0200

    g

--- zad3/src/Figury.java
+++ zad3/src/Figury.java
@@ -1,44 +1,54 @@
-public class Figury {
+class UjemnyAgrumentExeption extends Exception{};
+public class Figury{
     interface JednoArg {
-        public double ObliczPole(int arg1);
-        public double ObliczObwod(int arg1);
+        
+        public double ObliczPole(int arg1) throws UjemnyAgrumentExeption;
+        public double ObliczObwod(int arg1) throws UjemnyAgrumentExeption;
     }
     
     interface DwuArg {
-        public double ObliczPole(int arg1, int arg2);
-        public double ObliczObwod(int arg1, int arg2);
+        public double ObliczPole(int arg1, int arg2) throws UjemnyAgrumentExeption;
+        public double ObliczObwod(int arg1, int arg2) throws UjemnyAgrumentExeption;
     }
 
     public enum JednoParametrowe implements JednoArg{
         OKRAG {
-            public double ObliczObwod(int arg1) {
+            public double ObliczObwod(int arg1) throws UjemnyAgrumentExeption {
+                if(arg1 < 0) throw new UjemnyAgrumentExeption();
                 return 2*Math.PI*arg1;
             }
-            public double ObliczPole(int arg1) {
+            public double ObliczPole(int arg1) throws UjemnyAgrumentExeption{
+                if(arg1 < 0) throw new UjemnyAgrumentExeption();
                 return Math.PI*arg1*arg1;
             }
         },
         KWADRAT {
-            public double ObliczObwod(int arg1) {
+            public double ObliczObwod(int arg1) throws UjemnyAgrumentExeption{
+                if(arg1 < 0) throw new UjemnyAgrumentExeption();
                 return 4 * arg1;
             }
-            public double ObliczPole(int arg1) {
+            public double ObliczPole(int arg1) throws UjemnyAgrumentExeption{
+                if(arg1 < 0) throw new UjemnyAgrumentExeption();
                 return arg1 * arg1;
             }
         },
         PIECIOKAT {
-            public double ObliczObwod(int arg1) {
+            public double ObliczObwod(int arg1) throws UjemnyAgrumentExeption{
+                if(arg1 < 0) throw new UjemnyAgrumentExeption();
                 return 5 * arg1;
             }
-            public double ObliczPole(int arg1) {
+            public double ObliczPole(int arg1) throws UjemnyAgrumentExeption{
+                if(arg1 < 0) throw new UjemnyAgrumentExeption();
                 return arg1 * arg1 * Math.sqrt(25 + 10 * Math.sqrt(5)) / 4;
             }
         },
         SZESCIOKAT {
-            public double ObliczObwod(int arg1) {
+            public double ObliczObwod(int arg1) throws UjemnyAgrumentExeption{
+                if(arg1 < 0) throw new UjemnyAgrumentExeption();
                 return 6 * arg1;
             }
-            public double ObliczPole(int arg1) {
+            public double ObliczPole(int arg1) throws UjemnyAgrumentExeption{
+                if(arg1 < 0) throw new UjemnyAgrumentExeption();
                 return (arg1 * arg1 * Math.sqrt(3) / 4) * 6;
             }
         };
@@ -47,18 +57,22 @@ public class Figury {
 
     public enum DwuParametrowe implements DwuArg {
         PROSTOKAT {
-            public double ObliczObwod(int arg1, int arg2) {
+            public double ObliczObwod(int arg1, int arg2) throws UjemnyAgrumentExeption{
+                if(arg1 < 0 || arg2 <0 ) throw new UjemnyAgrumentExeption();
                 return (2 * arg1) + (2 * arg2);
             }
-            public double ObliczPole(int arg1, int arg2) {
+            public double ObliczPole(int arg1, int arg2) throws UjemnyAgrumentExeption{
+                if(arg1 < 0 || arg2 <0 ) throw new UjemnyAgrumentExeption();
                 return arg1 * arg2;
             }
         },
         ROMB {
-            public double ObliczObwod(int arg1, int arg2) {
+            public double ObliczObwod(int arg1, int arg2) throws UjemnyAgrumentExeption{
+                if(arg1 < 0 || arg2 <0 ) throw new UjemnyAgrumentExeption();
                 return 4 * arg1;
             }
-            public double ObliczPole(int arg1, int arg2) {
+            public double ObliczPole(int arg1, int arg2) throws UjemnyAgrumentExeption{
+                if(arg1 < 0 || arg2 <0 ) throw new UjemnyAgrumentExeption();
                 return Math.sin(Math.toRadians(arg2)) * arg1 * arg1;
             }
         };
@@ -80,10 +94,11 @@ public class Figury {
                     objekty[charInt][0] = JednoParametrowe.OKRAG.ObliczObwod(promien);
                     objekty[charInt][1] = JednoParametrowe.OKRAG.ObliczPole(promien);
                 }
-                catch (NumberFormatException ex) {
+                catch (NumberFormatException | UjemnyAgrumentExeption ex) {
                     System.out.println(args[pozycja] + " - nieprawidlowy promien");
                     System.exit(0);
                 }
+            
                 pozycja ++;
             }
 
@@ -113,7 +128,7 @@ public class Figury {
                         System.exit(0);
                     }
                 }
-                catch (NumberFormatException ex) {
+                catch (NumberFormatException | UjemnyAgrumentExeption ex) {
                     System.out.println("Jeden z bokow lub kat jest nieprawidlowy! ("+args[pozycja]+" "+args[pozycja+1]+" "+args[pozycja+2]+" "+args[pozycja+3]+" "+args[pozycja+4]+")");
                     System.exit(0);
                 }
@@ -126,7 +141,7 @@ public class Figury {
                     objekty[charInt][0] =JednoParametrowe.PIECIOKAT.ObliczObwod(bok1);
                     objekty[charInt][1] = JednoParametrowe.PIECIOKAT.ObliczPole(bok1);
                 }
-                catch (NumberFormatException ex) {
+                catch (NumberFormatException | UjemnyAgrumentExeption ex) {
                     System.out.println(args[pozycja] + " - nieprawidlowy bok");
                     System.exit(0);
                 }
@@ -139,7 +154,7 @@ public class Figury {
                     objekty[charInt][0] =JednoParametrowe.SZESCIOKAT.ObliczObwod(bok1);
                     objekty[charInt][1] = JednoParametrowe.SZESCIOKAT.ObliczPole(bok1);
                 }
-                catch (NumberFormatException ex) {
+                catch (NumberFormatException | UjemnyAgrumentExeption ex) {
                     System.out.println(args[0] + " - nieprawidlowy bok");
                     System.exit(0);
                 }
